c3923ab7ad2beaa726209e1d474c39517c5e8962
Victor Wernet <victor_w_ernet@hotmail.com>
2017-10-25 21:29:41 +0200

    Added smooth scrolling to slide

--- www/js/smoothScrolling.js
+++ www/js/smoothScrolling.js
@@ -1,1 +1,11 @@
-$(".nav").localScroll();
+//smooth scroll function for nav
+$('.nav').localScroll(/*{
+  duration: 1400,
+  easing: 'swing'
+}*/);
+
+//smooth scroll function for Get Started button
+$('.slideButtons').localScroll(/*{
+  duration: 1400,
+  easing: 'swing'
+}*/);
